d1cc19e14d5fa4f08a3fb494b4894604a012962d
BUAAer-xing <153857116+BUAAer-xing@users.noreply.github.com>
2023-12-14 12:43:36 -0600

    更新
    添加进度条，并基于csv存储文件进行下载，不需要每次下载均请求一次

--- download_matrix.py
+++ download_matrix.py
@@ -1,30 +1,28 @@
 import requests
-from bs4 import BeautifulSoup
-# 网址
-url = 'http://sparse.tamu.edu/?per_page=All'
+import csv
+from tqdm import tqdm
 
-# 发送请求获取网页内容
-response = requests.get(url)
-web_content = response.content
+all_lists = {}
+with open(file='all_matrix_information.csv', mode='r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)
+    for row in reader:
+        all_lists[row[0]] = row[1]
 
-# 解析网页
-soup = BeautifulSoup(web_content, 'html.parser')
+with_limitation_lists = []
+with open(file='with_limitation_matrix_information.csv',mode='r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)
+    for row in reader:
+        with_limitation_lists.append(row[1])
 
-download_links = soup.find_all(name='a', class_='btn btn-outline-dark btn-sm')
-download_links_MM = []
-file_name_and_url = {}
-
-for link in download_links:
-    if link.getText(strip=True) == 'Matrix Market':
-        download_links_MM.append(link.attrs['href'])
-        file_name_and_url[link.attrs['href'].split('/')[-1]] = link.attrs['href']
-
-for link in download_links_MM[0:1]:
-    # 下载文件
-    if link :
+for filename in tqdm(with_limitation_lists[0:4]):
+    if all_lists[filename]:
+        link = all_lists[filename]
         response = requests.get(link)
         # 你可能需要从URL中提取文件名
-        file_name = link.split('/')[-1]
+        file_name = 'download_matrix/'+link.split('/')[-1]
         # 保存文件
         with open(file_name, 'wb') as file:
             file.write(response.content)
+
